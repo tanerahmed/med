9ee16226dfadf2a42f9053485e833831ca306387
Taner Ahmed <tanerahmed87@gmail.com>
2024-01-19 09:51:17 +0200

    footer done

--- resources/views/layouts/admin_footer.blade.php
+++ resources/views/layouts/admin_footer.blade.php
@@ -1,4 +1,26 @@
-<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+{{-- <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
           <p class="text-muted mb-1 mb-md-0">Copyright © 2024 <a href="https://www.nobleui.com" target="_blank">NobleUI</a>.</p>
+        <p>asdadas</p>
+        <p>asdadas</p>
+        <p>asdadas</p>
+        <p>asdadas</p>
+
           <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
-      </footer>
+      </footer> --}}
+
+      <footer class="footer d-flex align-items-center justify-content-center">
+        <p class="text-muted mb-0">Copyright © 2024 <a href="https://zaracomputers.bg" target="_blank">Zara Computerss</a>.</p>
+        <p class="text-muted mb-0 ms-2">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+        
+        <button class="btn btn-link text-muted mb-0"><a href="#" target="_blank">GDPR</a></button>
+        <button class="btn btn-link text-muted mb-0"><a href="#" target="_blank">Editorial Publishing Practice</a></button>
+        <button class="btn btn-link text-muted mb-0"><a href="#" target="_blank">Contact Us</a></button>
+    </footer>
+    
+ 
+    
+    
+    
+    
+    
+    
