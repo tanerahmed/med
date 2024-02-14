a7d23589f4f89b1184d231f39378150ce2ebc364
Taner Ahmed <tanerahmed87@gmail.com>
2024-02-14 15:37:38 +0200

    Create Front end site begin

---
+++ app/Http/Controllers/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class Dashboard extends Controller
+{
+
+    public function index()
+    {
+        // Извличане на текущия логнат потребител
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return redirect()->route('admin.dashboard');
+        } elseif ($user->role === 'author') {
+            return redirect()->route('author.dashboard');
+        } elseif ($user->role === 'reviewer') {
+            return redirect()->route('reviewer.dashboard');
+        } else{
+            abort(404);
+        }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthorController;
@@ -25,15 +26,14 @@ use App\Http\Controllers\XMLController;
 */
 
 
-// !!!!! TODO REMOVE !!!!!
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('canva.home');
 });
 
-// !!!!! TODO REMOVE !!!!!
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [Dashboard::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
