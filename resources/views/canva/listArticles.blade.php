f4defaec770c9f48b41f2e4f03099c23002a4ef1
Taner Ahmed <tanerahmed87@gmail.com>
2024-03-25 08:44:37 +0200

    m

--- resources/views/canva/listArticles.blade.php
+++ resources/views/canva/listArticles.blade.php
@@ -1,7 +1,7 @@
 @extends('canva.home')
 @section('content')
     <!-- Page Title
-          ============================================= -->
+              ============================================= -->
     <section class="page-title bg-transparent">
         <div class="container">
             <div class="page-title-row">
@@ -22,18 +22,18 @@
     </section><!-- .page-title end -->
 
     <!-- Content
-          ============================================= -->
+              ============================================= -->
     <section id="content">
         <div class="content-wrap">
             <div class="container">
 
                 <div class="row gx-5 col-mb-80">
                     <!-- Post Content
-              ============================================= -->
+                  ============================================= -->
                     <main class="postcontent col-lg-9 order-lg-last">
 
                         <!-- Posts
-               ============================================= -->
+                   ============================================= -->
                         <div id="posts" class="row gutter-40">
                             @if ($articles->count())
                                 @foreach ($articles as $article)
@@ -41,21 +41,32 @@
                                         <div class="grid-inner row g-0">
                                             <div class="col-md-4">
                                                 <div class="entry-image">
-                                                    <a href={{ route('canva.showArticle', $article->id)}}>
-                                                    <img src="{{ asset('storage/' . $article->figures[0]->file_path) }}" alt="" style="width: 200px !important; ">
-                                                </a>
-                                                  </div>
+                                                    <a href={{ route('canva.showArticle', $article->id) }}>
+                                                        @if($article->figures->count() > 0)
+                                                        <img src="{{ asset('storage/' . $article->figures[0]->file_path) }}" alt="" style="width: 200px !important;">
+                                                    @else
+                                                        <img src="{{ asset('storage/no-img.jpg') }}" alt="Default Image" style="width: 200px !important;">
+                                                    @endif
+                                                    </a>
+                                                </div>
                                             </div>
                                             <div class="col-md-8 ps-md-4">
                                                 <div class="entry-title title-sm">
-                                                    <h2><a href={{ route('canva.showArticle', $article->id)}}>{{ $article->title }}</a></h2>
+                                                    <h2><a
+                                                            href={{ route('canva.showArticle', $article->id) }}>{{ $article->title }}</a>
+                                                    </h2>
                                                 </div>
                                                 <div class="entry-meta">
                                                     <ul>
-                                                        <li><i class="uil uil-schedule"></i>{{ $article->updated_at->format('Y-m-d') }}</li>
+                                                        <li><i
+                                                                class="uil uil-schedule"></i>{{ $article->updated_at->format('Y-m-d') }}
+                                                        </li>
                                                         <li><i class="uil uil-user"></i>{{ $article->user->name }}</li>
-                                                        <li><i class="uil uil-folder-open"></i>{{ $article->specialty }}</li>
-                                                        <li><i class="uil uil-folder-open"></i>{{ $article->scientific_area }}</li>
+                                                        <li><i class="uil uil-folder-open"></i>{{ $article->specialty }}
+                                                        </li>
+                                                        <li><i
+                                                                class="uil uil-folder-open"></i>{{ $article->scientific_area }}
+                                                        </li>
                                                         {{-- <li><a href="blog-single-full.html#comments"><i
                                                                 class="uil uil-comments-alt"></i> 19</a></li>
                                                     <li><a href="#"><i class="uil uil-film"></i></a></li> --}}
@@ -87,7 +98,7 @@
                     </main><!-- .postcontent end -->
 
                     <!-- Sidebar
-              ============================================= -->
+                  ============================================= -->
                     <aside class="sidebar col-lg-3">
                         <div class="sidebar-widgets-wrap">
 
@@ -113,8 +124,4 @@
 @endsection
 
 
-<script>
-
-
-
-</script>
+<script></script>
