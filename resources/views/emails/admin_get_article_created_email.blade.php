281c5df4348eaa9f90e54849fa99726ed303cd9b
Taner Ahmed <tanerahmed87@gmail.com>
2024-04-30 13:34:22 +0300

    m

--- resources/views/emails/admin_get_article_created_email.blade.php
+++ resources/views/emails/admin_get_article_created_email.blade.php
@@ -4,12 +4,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Co-Author Request Email</title>
+    <title>{{ $body['name'] }} - Created Article</title>
 </head>
 <body>
     <p>Hello, Admin</p>
     <p>The author: {{ $body['name'] }} created article with title: {{ $body['title'] }}</p>
-
+    <p> Go to <a href="https://blmprime.com/admin/dashboard">Dashboard</a></p>
     <p>Sincerely,</p>
     <p>Your Team</p>
 </body>
