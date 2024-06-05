6255bf68aadacf27303cd875c5df6c9d8d8d0feb
Taner Ahmed <tanerahmed87@gmail.com>
2024-06-05 15:01:50 +0300

    m

--- resources/views/canva/listArticles.blade.php
+++ resources/views/canva/listArticles.blade.php
@@ -39,7 +39,7 @@
                                 @foreach ($articles as $article)
                                     <div class="entry col-12">
                                         <div class="grid-inner row g-0">
-                                            <div class="col-md-4">
+                                            <div class="col-md-2">
                                                 <div class="entry-image">
                                                     <a href={{ route('canva.showArticle', $article->id) }}>
                                                         @if ($article->figures->count() > 0)
@@ -52,7 +52,7 @@
                                                     </a>
                                                 </div>
                                             </div>
-                                            <div class="col-md-8 ps-md-4">
+                                            <div class="col-md-10 ps-md-4">
                                                 <div class="entry-title title-sm">
                                                     <h2><a
                                                             href={{ route('canva.showArticle', $article->id) }}>{{ $article->title }}</a>
@@ -63,22 +63,25 @@
                                                         <li><i
                                                                 class="uil uil-schedule"></i>{{ $article->updated_at->format('Y-m-d') }}
                                                         </li>
-                                                        <li><i class="uil uil-user"></i>{{ $article->user->name }}</li>
+                                                       
                                                         <li><i class="uil uil-folder-open"></i>{{ $article->specialty }}
                                                         </li>
                                                         <li><i
                                                                 class="uil uil-folder-open"></i>{{ $article->scientific_area }}
                                                         </li>
-                                                        {{-- https://www.facebook.com/taner.ahmed --}}
-                                                        {{-- <li><a href="blog-single-full.html#comments"><i
-                                                                class="uil uil-comments-alt"></i> 19</a></li>
-                                                    <li><a href="#"><i class="uil uil-film"></i></a></li> --}}
+                                                    </ul>
+                                                    <br>
+                                                    <ul>
+                                                        <li><i class="uil uil-user"></i>{{ $article->user->name }}</li>
                                                         @foreach ($article->authors as $author)
                                                             @if ($author)
                                                                 <li><i class="uil uil-user"></i>{{ $author->first_name }}
                                                                     {{ $author->family_name }}</li>
                                                             @endif
                                                         @endforeach
+                                                    </ul>
+                                                    <br>
+                                                    <ul>
                                                         @if ($article->final_article_path)
                                                             <li>
                                                                 <a href="https://blmprime.com/storage/{{ $article->final_article_path }}"
@@ -86,23 +89,19 @@
                                                                     <button type="button"
                                                                         class="btn btn-success btn-sm">Download</button>
                                                                 </a>
-                                                                {{-- <a
-                                                                href="{{ route('admin.downolad_summary_pdf', $article->id) }}"><button
-                                                                    type="button"
-                                                                    class="btn btn-success btn-sm">PDF</button></a> --}}
                                                             </li>
                                                         @endif
                                                     </ul>
                                                 </div>
                                             </div>
-                                            <div class="col-md-8 ps-md-4">
+                                            {{-- <div class="col-md-8 ps-md-4">
                                                 <div class="entry-meta">
                                                     <ul>
 
 
                                                     </ul>
                                                 </div>
-                                            </div>
+                                            </div> --}}
                                         </div>
                                     </div>
                                 @endforeach
