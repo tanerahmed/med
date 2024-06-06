c7ad2d6dcffd2a3b624c23674ed509acccc5a0fd
Taner Ahmed <tanerahmed87@gmail.com>
2024-06-06 14:03:08 +0300

    m

--- resources/views/canva/showArticle.blade.php
+++ resources/views/canva/showArticle.blade.php
@@ -1,4 +1,4 @@
-@extends('canva.home')
+{{-- @extends('canva.home')
 @section('content')
     <!-- Document Wrapper ============================================= -->
     <div id="wrapper">
@@ -15,11 +15,11 @@
                         </main><!-- .postcontent end -->
 
                         <!-- Sidebar ============================================= -->
-                        <aside class="sidebar col-lg-3">
+                        <aside class="sidebar col-lg-3 custom-sidebar">
                             <div class="sidebar-widgets-wrap">
-                                <div class="widget tagcloud-widget" id="tagcloud-widget">
+                                <div class="widget tagcloud-widget custom-tagcloud-widget" id="tagcloud-widget">
                                     <h4>Contents</h4>
-                                    <div class="tagcloud">
+                                    <div class="tagcloud custom-tagcloud">
                                         <a href="#article-title">Article title</a>
                                         <a href="#abstract">Abstract</a>
                                         <a href="#keywords">Keywords</a>
@@ -27,22 +27,21 @@
                                         <a href="#manuscript">Manuscript</a>
                                         <a href="#supplementary">Supplementary Files</a>
                                         @if ($article->final_article_path)
-                                        <a href="https://blmprime.com/storage/{{ $article->final_article_path }}" download>
-                                            <button type="button" class="btn btn-success btn-sm">Download</button>
-                                        </a>                                       
+                                            <a href="https://blmprime.com/storage/{{ $article->final_article_path }}" download class="btn btn-success btn-sm custom-button">
+                                                Download
+                                            </a>
                                         @endif
 
                                         @if ($article->xmlFiles->isNotEmpty())
-                                        <a href="{{ route('download.latest.xml.for.article', ['articleId' => $article->id]) }}">
-                                            <button type="button" class="btn btn-primary btn-sm">Download XML</button>
-                                        </a>                                        
+                                            <a href="{{ route('download.latest.xml.for.article', ['articleId' => $article->id]) }}" class="btn btn-primary btn-sm custom-button">
+                                                Download XML
+                                            </a>
                                         @endif
-
                                     </div>
                                 </div>
 
                                 @if (isset($article->figures))
-                                    <div class="widget">
+                                    <div class="widget custom-gallery-widget">
                                         <h4>Gallery</h4>
                                         <div class="row">
                                             @foreach ($article->figures as $file)
@@ -64,15 +63,15 @@
             </div>
         </section><!-- #content end -->
     </div><!-- #wrapper end -->
-<style>
+
+    <style>
         /* Стилове за котвите */
         :target {
             scroll-margin-top: 100px;
         }
 
-
-        /* Предварителен стил за позицията */
-        .tagcloud-widget.fixed {
+        /* Стилове за tagcloud-widget */
+        .custom-tagcloud-widget.fixed {
             position: fixed;
             top: 20px;
             left: 50%;
@@ -88,17 +87,15 @@
         }
 
         /* Подреждане на елементите в tagcloud на един ред */
-        .tagcloud {
+        .custom-tagcloud {
             display: flex;
-            flex-wrap: wrap;
+            flex-direction: column;
             gap: 10px;
             /* разстояние между елементите */
-            justify-content: center;
-            /* Центриране на елементите вътре в tagcloud */
         }
 
-        /* .tagcloud a, */
-        .tagcloud button {
+        .custom-tagcloud a, 
+        .custom-tagcloud button {
             background-color: #198754;
             white-space: nowrap;
             color: #fff;
@@ -107,10 +104,24 @@
             /* Премахване на подчертаването на линковете */
         }
 
-        .tagcloud a:hover,
-        .tagcloud button:hover {
+        .custom-tagcloud a:hover,
+        .custom-tagcloud button:hover {
             text-decoration: underline;
             /* Подчертаване на линковете при hover за по-голяма видимост */
+        }
+
+        .custom-tagcloud .custom-button {
+            margin-top: 10px;
+        }
+
+        /* Стилове за sidebar */
+        .custom-sidebar {
+            /* Можете да добавите специфични стилове за сайдбара тук */
+        }
+
+        /* Стилове за gallery-widget */
+        .custom-gallery-widget {
+            /* Можете да добавите специфични стилове за галерията тук */
         }
     </style> 
 
@@ -128,4 +139,168 @@
             });
         });
     </script>
+@endsection --}}
+
+
+@extends('canva.home')
+@section('content')
+    <!-- Document Wrapper ============================================= -->
+    <div id="wrapper">
+        <section id="content">
+            <div class="content-wrap">
+                <div class="container">
+                    <div class="row gx-5 col-mb-80">
+                        <!-- Post Content ============================================= -->
+                        <main class="postcontent col-lg-9">
+                            <div class="single-post mb-0">
+                                <!-- Single Post Content Here -->
+                                {!! $content !!}
+                            </div>
+                        </main><!-- .postcontent end -->
+
+                        <!-- Sidebar ============================================= -->
+                        <aside class="sidebar col-lg-3 custom-sidebar">
+                            <div class="sidebar-widgets-wrap">
+                                <div class="widget tagcloud-widget custom-tagcloud-widget" id="tagcloud-widget">
+                                    <h4>Contents</h4>
+                                    <div class="tagcloud custom-tagcloud" id="tagcloud">
+                                        <a href="#article-title">Article title</a>
+                                        <a href="#abstract">Abstract</a>
+                                        <a href="#keywords">Keywords</a>
+                                        <a href="#title_page">Title Page</a>
+                                        <a href="#manuscript">Manuscript</a>
+                                        <a href="#supplementary">Supplementary Files</a>
+                                        @if ($article->final_article_path)
+                                            <a href="https://blmprime.com/storage/{{ $article->final_article_path }}" download class="btn btn-success btn-sm custom-button">
+                                                Download
+                                            </a>
+                                        @endif
+
+                                        @if ($article->xmlFiles->isNotEmpty())
+                                            <a href="{{ route('download.latest.xml.for.article', ['articleId' => $article->id]) }}" class="btn btn-primary btn-sm custom-button">
+                                                Download XML
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                @if (isset($article->figures))
+                                    <div class="widget custom-gallery-widget">
+                                        <h4>Gallery</h4>
+                                        <div class="row">
+                                            @foreach ($article->figures as $file)
+                                                <div class="col-6 col-sm-6 mb-3">
+                                                    <a href="{{ 'https://blmprime.com/storage/' . $file->file_path }}"
+                                                        data-lightbox="gallery" data-title="Image 1">
+                                                        <img src="{{ 'https://blmprime.com/storage/' . $file->file_path }}"
+                                                            class="img-fluid rounded" alt="Image 1">
+                                                    </a>
+                                                </div>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
+                        </aside><!-- .sidebar end -->
+                    </div>
+                </div>
+            </div>
+        </section><!-- #content end -->
+    </div><!-- #wrapper end -->
+
+    <style>
+        /* Стилове за котвите */
+        :target {
+            scroll-margin-top: 100px;
+        }
+
+        /* Стилове за tagcloud-widget */
+        .custom-tagcloud-widget.fixed {
+            position: fixed;
+            top: 20px;
+            left: 50%;
+            transform: translateX(-50%);
+            background-color: white;
+            border: 1px solid #ddd;
+            padding: 10px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Подреждане на елементите в tagcloud на един ред */
+        .custom-tagcloud {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        .custom-tagcloud.fixed-layout {
+            flex-direction: row;
+        }
+
+        .custom-tagcloud a, 
+        .custom-tagcloud button {
+            /* background-color: #198754; */
+            white-space: nowrap;
+            color: black;
+            /* color: #fff; */
+            text-decoration: none;
+        }
+
+        .custom-tagcloud a:hover,
+        .custom-tagcloud button:hover {
+            text-decoration: underline;
+        }
+
+        .custom-tagcloud .custom-button {
+            margin-top: 10px;
+        }
+
+        /* Стилове за sidebar */
+        .custom-sidebar {
+            /* Можете да добавите специфични стилове за сайдбара тук */
+        }
+
+        /* Стилове за gallery-widget */
+        .custom-gallery-widget {
+            /* Можете да добавите специфични стилове за галерията тук */
+        }
+ /* Медийна заявка за мобилни устройства */
+ @media (max-width: 767px) {
+            .custom-tagcloud-widget.fixed {
+                position: static;
+                transform: none;
+                box-shadow: none;
+                border: none;
+                padding: 0;
+            }
+
+            .custom-tagcloud.fixed-layout {
+                flex-direction: column;
+            }
+
+            .custom-tagcloud .custom-button {
+                margin-top: 10px;
+            }
+        }
+
+    </style> 
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const tagcloudWidget = document.getElementById('tagcloud-widget');
+            const tagcloud = document.getElementById('tagcloud');
+            const offsetTop = tagcloudWidget.offsetTop;
+
+            window.addEventListener('scroll', function() {
+                if (window.pageYOffset > offsetTop) {
+                    tagcloudWidget.classList.add('fixed');
+                    tagcloud.classList.add('fixed-layout');
+                } else {
+                    tagcloudWidget.classList.remove('fixed');
+                    tagcloud.classList.remove('fixed-layout');
+                }
+            });
+        });
+    </script>
 @endsection
+
