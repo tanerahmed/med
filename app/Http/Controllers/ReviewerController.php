0c8371947da280c7d6cc621123c71ebc1ced47b7
Taner Ahmed <tanerahmed87@gmail.com>
2024-06-11 08:51:37 +0300

    m

--- app/Http/Controllers/ReviewerController.php
+++ app/Http/Controllers/ReviewerController.php
@@ -188,27 +188,27 @@ class ReviewerController extends Controller
         // Проверка на комбинацията от рейтинги
         $ratings = [$review->rating_1, $review->rating_2, $review->rating_3];
         if (in_array('accepted', $ratings) && in_array('accepted with revision', $ratings)) {            
-            $article->author_can_edit = 1;
+            // $article->author_can_edit = 1;
             $article->status = 'accepted';
             $article->save();
         } elseif (in_array('declined', $ratings) && in_array('acceptedn', $ratings)) {
-            $article->author_can_edit = 1;
+            // $article->author_can_edit = 1;
             $article->status = 'pending';
             $article->save();
         } elseif (in_array('declined', $ratings) && in_array('accepted with revision', $ratings)) {
-            $article->author_can_edit = 1;
+            // $article->author_can_edit = 1;
             $article->status = 'pending';
             $article->save();
         } elseif (in_array('declined', $ratings) && in_array('declined', $ratings)) {
-            $article->author_can_edit = 1;
+            // $article->author_can_edit = 1;
             $article->status = 'declined';
             $article->save();
         } elseif (count(array_filter($ratings, fn($rating) => $rating === 'accepted with revision')) >= 2) {
-            $article->author_can_edit = 1;
+            // $article->author_can_edit = 1;
             $article->status = 'pending';
             $article->save();
         }else{
-            $article->author_can_edit = 0;
+            // $article->author_can_edit = 0;
             $article->save();
         }
 
